1640430a0e08846eec9f7e728b33aa8c7862a3fa
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-05-23 15:55:24 -0700

    cmp - rebuild site

--- public/examples/ex9/test.tcl
+++ public/examples/ex9/test.tcl
@@ -3,10 +3,10 @@ source LibUnits.tcl
 foreach model {
     Ex9a.build.UniaxialSection2D.tcl
     Ex9b.build.WSection2D.tcl
-    Ex9c.build.RCSection.RectUnconfinedSymm2D.tcl
-    Ex9d.build.RCSection.RectConfinedSymm2D.tcl
-    Ex9e.build.RCSection.Rect2D.tcl
-    Ex9f.build.RCSection.Circ2D.tcl
+    Ex9c.build.RectUnconfinedSymm2D.tcl
+    Ex9d.build.RectConfinedSymm2D.tcl
+    Ex9e.build.Rect2D.tcl
+    Ex9f.build.Circ2D.tcl
   } {
     wipe
     puts "Model: $model"
@@ -18,10 +18,10 @@ foreach model {
 foreach model {
     Ex9a.build.UniaxialSection3D.tcl
     Ex9b.build.WSection3D.tcl
-    Ex9c.build.RCSection.RectUnconfinedSymm3D.tcl
-    Ex9d.build.RCSection.RectConfinedSymm3D.tcl
-    Ex9e.build.RCSection.Rect3D.tcl
-    Ex9f.build.RCSection.Circ3D.tcl
+    Ex9c.build.RectUnconfinedSymm3D.tcl
+    Ex9d.build.RectConfinedSymm3D.tcl
+    Ex9e.build.Rect3D.tcl
+    Ex9f.build.Circ3D.tcl
     Ex9g.build.HollowSection3D.tcl
   } {
     wipe
---
+++ public/examples/framebuckling/EulerBuckling.py
@@ -0,0 +1,95 @@
+from math import cos,sin,sqrt,pi
+import opensees.openseespy as ops
+
+E = 29000.0
+I = 110.0
+A = 9.12e3
+L = 60.0
+
+NumberOfElements = 10
+ElementType      = "ForceBeamColumn"
+GeomTransfType   = "Corotational"
+
+LoadStep      = 0.01
+PeakLoadRatio = 2.00
+
+# END OF INPUT
+
+NumIntegrationPoints = 3
+NumberOfNodes = NumberOfElements+1
+EulerLoad     = (pi*pi*E*I/(L*L))
+
+model = ops.Model(ndm=2,  ndf=3)
+
+# Define Nodes
+for i in range(1, NumberOfNodes+1):
+    y = ((i-1)/float(NumberOfElements)*L)
+    model.node(i, 0.0, y)
+    model.mass(i, 1.0, 1.0, 1.0)
+
+
+# Define Boundary Conditions
+model.fix(            1, 1, 1, 0)
+model.fix(NumberOfNodes, 1, 0, 0)
+
+# Define Section 
+SectionTag = 1
+model.section('Elastic', SectionTag, E, A, I)
+
+# Define Geometric Transformation
+GeomTransfTag = 1
+model.geomTransf(GeomTransfType, GeomTransfTag)
+
+# Define Elements
+model.eval("pragma openseespy off")
+for i in range(1, NumberOfElements+1):
+    model.element(ElementType, i, i, i+1, NumIntegrationPoints, SectionTag, GeomTransfTag)
+model.eval("pragma openseespy on")
+
+# Apply loads
+model.pattern('Plain', 1, "Linear")
+model.load(NumberOfNodes, 0.0, -EulerLoad, 0.0, pattern=1)
+
+
+# Analysis Options
+model.system('UmfPack')
+model.constraints('Transformation')
+model.test('NormUnbalance', 1.0e-6, 20, 0)
+model.algorithm('Newton', )
+model.numberer('Plain')
+model.integrator('LoadControl', LoadStep)
+model.analysis('Static')
+
+LastLoadRatio =  model.getTime()
+LastEigenvalue = model.eigen( 1)
+
+success = False
+for i in range(1, int(PeakLoadRatio/LoadStep)+1):
+    model.analyze(1)
+    CurrentLoadRatio =   model.getTime()
+    CurrentEigenvalue =  model.eigen(1)[0]
+
+    if CurrentEigenvalue <= 0.0:
+        print("Analysis Finished")
+
+        InterpolatedLoadRatio = LastLoadRatio + (CurrentLoadRatio-LastLoadRatio)*LastEigenvalue/(LastEigenvalue-CurrentEigenvalue)
+
+        print(f"Limit Point Found")
+        print(f"Number Of Elements:              {NumberOfElements}")
+        print(f"Element Type:                    {ElementType}")
+        print(f"Geometric Transformation Type:   {GeomTransfType}")
+        print(f"Exact Euler Load:                {EulerLoad:.2f}")
+        print(f"Computed Euler Load:             {InterpolatedLoadRatio*EulerLoad:.2f}")
+        print(f"Percent Error:                   {100*(InterpolatedLoadRatio-1):.2f}")
+        success = True
+        break
+
+
+    LastLoadRatio =  CurrentLoadRatio
+    LastEigenvalue = CurrentEigenvalue
+
+
+
+print("Analysis Finished")
+if not success:
+    print("No Limit Point Found")
