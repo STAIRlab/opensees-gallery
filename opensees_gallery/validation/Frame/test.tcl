95d5f65be6832e3c798b4c331f42bb1ba59d3620
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-08-05 14:58:23 -0700

    cmp

--- opensees_gallery/validation/Frame/test.tcl
+++ opensees_gallery/validation/Frame/test.tcl
@@ -1,10 +1,8 @@
 wipe; source test_BeamLoad3D.tcl
 wipe; source test_BeamsAndSections3D.tcl
 wipe; source test_ShearBeam3D.tcl
-wipe; source test_section.tcl
 
-# taper
-# inelastic
+#wipe; source test_section.tcl
 
 # wipe; source test_EigenFrame.tcl
 # wipe; source test_BeamLoad2D.tcl
