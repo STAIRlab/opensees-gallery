1b1ed2d0d9bb9692ac3ee9797c0237859e8c7050
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-08-28 22:37:09 -0700

    Update test-arch.tcl

--- content/en/benchmarks/solve-0003/test-arch.tcl
+++ content/en/benchmarks/solve-0003/test-arch.tcl
@@ -11,10 +11,12 @@ node  8 2011.1741391101934 419.7770864617414 0
 node  9 3012.765365914115 319.6179637813475 0 
 node 10 4009.568694135459 179.6181055187626 0 
 node 11 5000.0 0.0 0 
+fix  1 1 1 0 1 1 0 
+fix 11 1 1 0 1 1 0 
 
 section ElasticFrame 1 -A 10000.0 -E 200 -Iy 100000000.0 -Iz 100000000.0 -J 200000000.0 -G 200 -Ay 1000000.0 -Az 1000000.0
 
-geomTransf Isometric 1 0 0 1 
+geomTransf Corotational02 1 0 0 1 
 
 element ForceFrame  1  1  2 -section 1 -transform 1
 element ForceFrame  2  2  3 -section 1 -transform 1
@@ -26,8 +28,6 @@ element ForceFrame  7  7  8 -section 1 -transform 1
 element ForceFrame  8  8  9 -section 1 -transform 1
 element ForceFrame  9  9 10 -section 1 -transform 1
 element ForceFrame 10 10 11 -section 1 -transform 1
-fix  1 1 1 0 1 1 0 
-fix 11 1 1 0 1 1 0 
 
 fix  1 -dof 3
 fix  2 -dof 3
@@ -42,8 +42,9 @@ fix 10 -dof 3
 fix 11 -dof 3
 pattern Plain 1 Linear 
 nodalLoad 6 0.0 -1.0 0.0 0 0 0 -pattern 1 
+
 system BandGeneral -det
-test NormDispIncr 1e-08 25 1 
+test NormDispIncr 1e-08 8 0
 algorithm Newton 
 analysis Static
 integrator ArcLength 45 0 -det -exp 0.5 -reference point
